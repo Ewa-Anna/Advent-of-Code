e168decaa6b9c75a0191429724ddcdc50e0b50f5
Ewa-Anna <ewaanna073@gmail.com>
2023-12-09 19:55:30 +0100

    AoC 2023 Day 09

---
+++ 2022/Day_01/day_01_part_1.py
@@ -0,0 +1,16 @@
+data = "input.txt"
+
+with open(data) as f:
+    data = f.read()
+
+groups = data.strip().split('\n\n')
+source = [list(map(int, group.split('\n'))) for group in groups]
+
+output = []
+for line in source:
+    summa = sum(line)
+    output.append(summa)
+    maximum = max(output)
+
+
+print(maximum)
