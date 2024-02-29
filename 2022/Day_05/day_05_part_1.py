ad5b418d0abe1e95d5ace518adda014689b56154
unknown <ewaanna073@gmail.com>
2024-02-29 19:51:44 +0100

    Formatting

--- 2022/Day_05/day_05_part_1.py
+++ 2022/Day_05/day_05_part_1.py
@@ -9,12 +9,15 @@ crates = []
 for line in crates_data:
     crate = []
     for char in line:
-        if char != '[' and char != ']':
+        if char != "[" and char != "]":
             crate.append(char)
     crates.append(crate)
 crates.pop()  # Remove last row with number of crate placing
 
 
-crates = ["".join(col[-2::-1]).strip() for idx,
-          col in enumerate(zip_longest(*crates_data, fillvalue='')) if idx % 4 == 1]
+crates = [
+    "".join(col[-2::-1]).strip()
+    for idx, col in enumerate(zip_longest(*crates_data, fillvalue=""))
+    if idx % 4 == 1
+]
 crates = [item[::1] for item in crates]
