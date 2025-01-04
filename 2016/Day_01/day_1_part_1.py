7e247fa3c6a8225aebe367c8e0ad29461dfc8b6b
unknown <ewaanna073@gmail.com>
2025-01-04 12:30:15 +0100

    AoC 2016 Day 01

---
+++ 2016/Day_01/day_1_part_1.py
@@ -0,0 +1,28 @@
+def calculate_shortest_path(filename):
+    with open(filename, 'r') as file:
+        directions = file.read().strip().split(', ')
+
+    x, y = 0, 0 
+    direction = 0  
+
+    for step in directions:
+        turn, distance = step[0], int(step[1:])
+
+        if turn == 'L':
+            direction = (direction - 1) % 4
+        elif turn == 'R':
+            direction = (direction + 1) % 4
+
+        if direction == 0:  # North
+            y += distance
+        elif direction == 1:  # East
+            x += distance
+        elif direction == 2:  # South
+            y -= distance
+        elif direction == 3:  # West
+            x -= distance
+
+    return abs(x) + abs(y)
+
+shortest_path = calculate_shortest_path('input.txt')
+print(f"The shortest path to Easter Bunny HQ is {shortest_path} blocks.")
